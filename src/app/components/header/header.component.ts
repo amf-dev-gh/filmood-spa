053aa4ff0776c254f5695bef0e007814162b7e8e
Andres Fernandez <andres94.dev@gmail.com>
2025-04-17 19:45:23 +0200

    creo el header responsive con su menu desplegable

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
 import { IconComponent } from "../icon/icon.component";
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [IconComponent, RouterLink],
-  templateUrl: './header.component.html'
+  imports: [IconComponent, RouterLink, CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  showMenu: boolean = false;
+
+  closeMenu() {
+    this.showMenu = false;
+  }
+
+  openClose(){
+    this.showMenu = !this.showMenu;
+  }
 
 }
