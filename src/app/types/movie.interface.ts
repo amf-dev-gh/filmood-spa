925f23e277c95811a2f6899c3be31da749065b57
Andres Fernandez <andres94.dev@gmail.com>
2025-04-18 22:04:22 +0200

    añado interfaz de compañia

--- src/app/types/movie.interface.ts
+++ src/app/types/movie.interface.ts
@@ -8,10 +8,11 @@ export interface Movie {
   poster_path: string,
   title: string,
   vote_average: number,
-  tagline:string,
-  genres:Genre[],
-  homepage:string,
-  backdrop_path:string
+  tagline: string,
+  genres: Genre[],
+  homepage: string,
+  backdrop_path: string,
+  production_companies: ProductionCompany[]
 }
 
 export interface Genre {
@@ -20,26 +21,31 @@ export interface Genre {
 }
 
 export interface Video {
-  key:string,
-  name:string,
-  official:boolean,
-  site:string,
-  type:string
+  key: string,
+  name: string,
+  official: boolean,
+  site: string,
+  type: string
 }
 
 export interface Image {
-  file_path:string,
-  height:number,
-  whidth:number
+  file_path: string,
+  height: number,
+  whidth: number
 }
 
 export interface Person {
-  id:number,
-  gender:number,
-  known_for_department:string,
-  original_name:string,
-  character:string,// nombre de personaje
-  profile_path:String,
-  job:string
-  name:string,
+  id: number,
+  gender: number,
+  known_for_department: string,
+  original_name: string,
+  character: string,// nombre de personaje
+  profile_path: String,
+  job: string
+  name: string,
+}
+
+export interface ProductionCompany {
+  logo_path: string,
+  name: string
 }
