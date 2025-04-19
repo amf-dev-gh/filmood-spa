c6e9d6c2473890d983090ba86ca5b53b91762cb5
Andres Fernandez <andres94.dev@gmail.com>
2025-04-19 23:53:42 +0200

    elimino pagina de categorias. implemento directo en pagina peliculas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,11 @@ import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { MoodsComponent } from './components/moods/moods.component';
-import { CategoriesComponent } from './components/categories/categories.component';
 import { AboutComponent } from './components/about/about.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'categories', component: CategoriesComponent },
   { path: 'about', component: AboutComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
