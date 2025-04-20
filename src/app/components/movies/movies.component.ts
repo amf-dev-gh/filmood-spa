663c3c983859bd8036cb94d4b134a9f6d5c6ac87
Andres Fernandez <andres94.dev@gmail.com>
2025-04-20 17:41:59 +0200

    implementacion principal en pagina de peliculas

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MovieService } from '../../services/movie.service';
+import { Movie } from '../../types/movie.interface';
 
 @Component({
   selector: 'app-movies',
-  imports: [],
+  imports: [CommonModule, FormsModule,],
   templateUrl: './movies.component.html'
 })
 export class MoviesComponent {
+
+  private readonly movieService = inject(MovieService);
+
+  inputTitle: string = '';
+
+  foundMovies: Movie[] = [];
+
+  searchMovie() {
+    if (this.inputTitle === '') {
+      // no puede ser vacio
+      return;
+    }
+    if (this.inputTitle.length < 4) {
+      // igrese al menos 3 letras
+      return
+    }
+
+    console.log('Buscar por ', this.inputTitle);
+    this.movieService.findMovieByTitle(this.inputTitle).subscribe({
+      next: response => {
+        console.log(response);
+        this.foundMovies = response.results;
+      },
+      error: err => console.error("Error searching movies", err)
+    })
+    this.inputTitle = '';
+  }
 
 }
