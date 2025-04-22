40fbbc7d4d83cf5f957f39a5593a9d5c206ab422
Andres Fernandez <andres94.dev@gmail.com>
2025-04-22 19:19:04 +0200

    implementados metodos para crear, eliminar y obtener mods de un usuario

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -11,8 +11,16 @@ export class ApiService {
   private readonly http = inject(HttpClient);
   private API_URL: string = 'http://localhost:8080/api/filmood';
 
-  getUserMoods(username:string):Observable<UserMood[]>{
-    return this.http.get<UserMood[]>(`${this.API_URL}/moods/${username}`);
+  getUserMoods(): Observable<UserMood[]> {
+    return this.http.get<UserMood[]>(`${this.API_URL}/moods/user`);
+  }
+
+  createNewMood(name:string):Observable<any>{
+    return this.http.post<any>(`${this.API_URL}/moods/new/${name}`,{});
+  }
+
+  deleteMood(id:number):Observable<any>{
+    return this.http.delete<any>(`${this.API_URL}/moods/delete/${id}`);
   }
 
 }
