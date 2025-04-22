0ddd20383ceb0fe79bc352b25cbee69c112cb8f9
Andres Fernandez <andres94.dev@gmail.com>
2025-04-22 19:18:06 +0200

    creado interceptor para JWT de la API

---
+++ src/app/services/jwt.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn } from "@angular/common/http";
+import { AuthService } from "./auth.service";
+import { inject } from "@angular/core";
+
+export const JwtInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  const routes = [
+    'http://localhost:8080/api/filmood',
+  ];
+
+  const requiereAuth = routes.some(url => req.url.startsWith(url));
+  if (requiereAuth) {
+    req = req.clone({ setHeaders: { Authorization: `Bearer ${token}` } });
+  }
+
+  return next(req);
+};
