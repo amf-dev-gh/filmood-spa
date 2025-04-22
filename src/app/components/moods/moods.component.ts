a98149a75dcc7e1eb05f6fa3904144c99475c5a2
Andres Fernandez <andres94.dev@gmail.com>
2025-04-22 19:27:39 +0200

    implemento funcionalidades crear eliminar y ver moods en el componente, aplicado el responsive

--- src/app/components/moods/moods.component.ts
+++ src/app/components/moods/moods.component.ts
@@ -4,10 +4,11 @@ import { AuthService } from '../../services/auth.service';
 import { UserMood } from '../../types/mood.interface';
 import { CommonModule } from '@angular/common';
 import { CardMovieComponent } from "../card-movie/card-movie.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-moods',
-  imports: [CommonModule, CardMovieComponent],
+  imports: [CommonModule, CardMovieComponent, FormsModule],
   templateUrl: './moods.component.html',
   styleUrl: './moods.component.css'
 })
@@ -18,16 +19,20 @@ export class MoodsComponent implements OnInit {
 
   readonly username = this.authService.getUsername();
 
-  userMoods:UserMood[] = [];
+  userMoods: UserMood[] = [];
 
-  selectedMood?:UserMood;
+  selectedMood?: UserMood;
+
+  showForm: boolean = false;
+
+  inputName: string = '';
 
   ngOnInit(): void {
     this.getUserMoods();
   }
 
-  getUserMoods(){
-    this.apiService.getUserMoods(this.username).subscribe({
+  getUserMoods() {
+    this.apiService.getUserMoods().subscribe({
       next: response => {
         this.userMoods = response;
       },
@@ -37,16 +42,48 @@ export class MoodsComponent implements OnInit {
     })
   }
 
-  selectMood(mood:UserMood){
+  selectMood(mood: UserMood) {
     this.selectedMood = mood;
   }
 
-  closeMood(){
+  closeMood() {
     this.selectedMood = undefined;
   }
 
-  createMood(){
-    
+  newMood() {
+    this.showForm = true;
+  }
+
+  closeNewMood() {
+    this.showForm = false;
+  }
+
+  createMood() {
+    if (this.inputName === '') {
+      return;
+    }
+    this.apiService.createNewMood(this.inputName).subscribe({
+      next: () => {
+        window.location.reload();
+      },
+      error: err => {
+        console.error("Error creating mood", err);
+      }
+    })
+  }
+
+  deleteMood(id: number) {
+    const confirmDelete = confirm('¿Está seguro de eliminiar el mod actual?');
+    if (confirmDelete) {
+      this.apiService.deleteMood(id).subscribe({
+        next: () => {
+          window.location.reload();
+        },
+        error: err => {
+          console.error("Error creating mood", err);
+        }
+      })
+    }
   }
 
 }
