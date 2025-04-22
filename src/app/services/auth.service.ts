b965b6e1455cdefe3e4f7f08b36ee7f62d6147e3
Andres Fernandez <andres94.dev@gmail.com>
2025-04-22 12:29:51 +0200

    creo servicio basico de autenticacion

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { inject, Injectable } from '@angular/core';
+import { LoginCredentials, LoginResponse, RegisterResponse, RegisterUser } from '../types/auth.interface';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private readonly http = inject(HttpClient);
+  private API_URL: string = 'http://localhost:8080/api/auth';
+  private USER: string = 'USER_NAME';
+  private ROLE: string = 'USER_ROLE';
+  private TOKEN: string = 'USER_TOKEN';
+  private EXPIRATION: string = 'EXPIRATION_TIME';
+
+
+  singUp(newUser: RegisterUser): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>(`${this.API_URL}/singup`, newUser);
+  }
+
+  login(credentials: LoginCredentials): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${this.API_URL}/login`, credentials);
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+
+  setUserStorage(response: LoginResponse) {
+    sessionStorage.setItem(this.TOKEN, response.token);
+    sessionStorage.setItem(this.USER, response.username);
+    sessionStorage.setItem(this.EXPIRATION, response.expirationTime.toString());
+    sessionStorage.setItem(this.ROLE, response.role);
+  }
+
+  getToken(): string | null {
+    const token = localStorage.getItem(this.TOKEN);
+    return token ? token : null;
+  }
+
+}
