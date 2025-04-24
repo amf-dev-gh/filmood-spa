5660691300478d9fd76777ef8101eb2c4768fb3d
Andres Fernandez <andres94.dev@gmail.com>
2025-04-24 14:25:35 +0200

    elimino variables innecesarias

--- src/app/components/moods/moods.component.ts
+++ src/app/components/moods/moods.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
-import { AuthService } from '../../services/auth.service';
 import { UserMood } from '../../types/mood.interface';
 import { CommonModule } from '@angular/common';
 import { CardMovieComponent } from "../card-movie/card-movie.component";
@@ -15,9 +14,6 @@ import { FormsModule } from '@angular/forms';
 export class MoodsComponent implements OnInit {
 
   private readonly apiService = inject(ApiService);
-  private readonly authService = inject(AuthService);
-
-  readonly username = this.authService.getUsername();
 
   userMoods: UserMood[] = [];
 
