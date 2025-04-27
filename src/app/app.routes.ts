4e808e343870f86fe7b7a0f22ef4e36a26c63ac3
Andres Fernandez <andres94.dev@gmail.com>
2025-04-27 18:42:58 +0200

    eliminado componente ComunityMoods y sus rutas, así como los componentes de autenticación y sus estilos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,20 +4,13 @@ import { MoviesComponent } from './components/movies/movies.component';
 import { MovieDetailsComponent } from './components/movie-details/movie-details.component';
 import { MoodsComponent } from './components/moods/moods.component';
 import { AboutComponent } from './components/about/about.component';
-import { LoginComponent } from './components/login/login.component';
-import { SingupComponent } from './components/singup/singup.component';
-import { LoginGuard } from './guards/login.guard';
-import { ComunityMoodsComponent } from './components/comunity-moods/comunity-moods.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'home', component: HomeComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'singup', component: SingupComponent },
   { path: 'about', component: AboutComponent },
   { path: 'movies', component: MoviesComponent },
   { path: 'movie/:id', component: MovieDetailsComponent },
-  { path: 'comunity', component: ComunityMoodsComponent },
-  { path: 'moods', component: MoodsComponent, canActivate: [LoginGuard] },
+  { path: 'moods', component: MoodsComponent },
   { path: '**', component: HomeComponent }
 ];
